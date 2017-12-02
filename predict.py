665ea8910a51c2a50026e15c7228cd562427a832
Ben Liu <benliu.1997@gmail.com>
2017-12-02 18:07:40 -0500

    added prediction code

---
+++ predict.py
@@ -0,0 +1,17 @@
+import numpy
+import pandas as pd
+from pandas.tseries.offsets import BDay
+from sklearn.neural_network import MLPClassifier
+import pickle
+
+percent_filename = 'percent_change.sav'
+change_filename = 'change.sav'
+
+percent_change_model = pickle.load(open(percent_filename, 'rb'))
+change_model = pickle.load(open(change_filename, 'rb'))
+
+def predict_percent(data):
+    return percent_change_model.predict(data)
+
+def predict_change(data):
+    return change_model.predict(data)
